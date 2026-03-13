867809e7ef2dd249288cd5876ded7fb5d33053a2
Boom-Promphan Suksumek <boom.promphans@gmail.com>
2026-03-13 23:25:11 +0700

    Update Ramsay Controller

--- helpers/ramsayController.py
+++ helpers/ramsayController.py
@@ -1,7 +1,12 @@
+"""
+ramsayController with glyph name autocomplete
+"""
+
+import objc
 import ezui
-from mojo.subscriber import (
-    Subscriber,
-    registerGlyphEditorSubscriber)
+from AppKit import NSObject
+from mojo.subscriber import Subscriber, registerGlyphEditorSubscriber
+from lib.tools.debugTools import ClassNameIncrementer
 
 try:
     from ramsayStData import RamsayStData
@@ -10,13 +15,40 @@ except ImportError:
 
 
 class DummyRamsayData(object):
-
     def __init__(self):
         self.data = dict()
 
 
-class RamsayController(ezui.WindowController):
+class ComboBoxDataSource(NSObject, metaclass=ClassNameIncrementer):
+    def init(self):
+        self = objc.super(ComboBoxDataSource, self).init()
+        self._glyphNames = []
+        return self
 
+    def setGlyphNames_(self, names):
+        self._glyphNames = names
+
+    def comboBox_completedString_(self, comboBox, text):
+        if text in self._glyphNames:
+            return text
+        for name in self._glyphNames:
+            if name.startswith(text):
+                return name
+        return text
+
+    def comboBox_indexOfItemWithStringValue_(self, comboBox, text):
+        if text not in self._glyphNames:
+            return -1
+        return self._glyphNames.index(text)
+
+    def comboBox_objectValueForItemAtIndex_(self, comboBox, index):
+        return self._glyphNames[index]
+
+    def numberOfItemsInComboBox_(self, comboBox):
+        return len(self._glyphNames)
+
+
+class RamsayController(ezui.WindowController):
     def build(self):
         try:
             from ramsayStData import RamsayStData
@@ -24,40 +56,41 @@ class RamsayController(ezui.WindowController):
             RamsayStData = DummyRamsayData()
 
         self.g = CurrentGlyph()
+        self.glyph_order = list(self.g.font.glyphOrder)
 
         if self.g.isEmpty():
             self.gname = None
         else:
             self.gname = self.g.name
-        gn_left, gn_right = RamsayStData.data.get(self.gname, ('n', 'n'))
+        gn_left, gn_right = RamsayStData.data.get(self.gname, ("n", "n"))
 
         content = f"""
         = HorizontalStack
-        [_ {gn_left} _] @left
+        [_ {gn_left} ...] @left
         {self.gname} @middle
-        [_ {gn_right} _] @right
+        [_ {gn_right} ...] @right
         (OK) @button
         """
 
         descriptionData = dict(
-            left=dict(
-                callback='fieldCallback',
-                continuous=True,
-            ),
-            right=dict(
-                callback='fieldCallback',
-                continuous=True,
-            ),
+            left=dict(width=180, items=self.glyph_order),
+            right=dict(width=180, items=self.glyph_order),
         )
-
         self.w = ezui.EZPanel(
             content=content,
-            controller=self,
             descriptionData=descriptionData,
-            size=(100, 'auto'),
+            controller=self,
+            size=(460, "auto"),
         )
+        self.data_source = ComboBoxDataSource.alloc().init()
+        self.data_source.setGlyphNames_(self.glyph_order)
+        for item_name in ("left", "right"):
+            item = self.w.getItem(item_name)
+            cb = item.getNSComboBox()
+            cb.setUsesDataSource_(True)
+            cb.setDataSource_(self.data_source)
 
-        button = self.w.getItem('button')
+        button = self.w.getItem("button")
         button.bind(chr(13), [])
         self.w.getNSWindow().setTitleVisibility_(False)
 
@@ -70,8 +103,8 @@ class RamsayController(ezui.WindowController):
         except ImportError:
             RamsayStData = DummyRamsayData()
 
-        gn_left = self.w.getItem('left').get()
-        gn_right = self.w.getItem('right').get()
+        gn_left = self.w.getItem("left").get()
+        gn_right = self.w.getItem("right").get()
         if RamsayStData.data.get(self.gname) != (gn_left, gn_right):
             RamsayStData.data[self.gname] = (gn_left, gn_right)
             RamsayStData.save()
@@ -95,9 +128,7 @@ class RamsayController(ezui.WindowController):
 
 
 class RamsayControllerMenuItem(Subscriber):
-
     def glyphEditorWantsContextualMenuItems(self, info):
-
         def _edit_ramsay(sender):
             RamsayController()
 
